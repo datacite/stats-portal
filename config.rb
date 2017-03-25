74ff21c18a6195e16f7f12e0a0ca1036051ee2e9
Martin Fenner <martin.fenner@datacite.org>
2017-03-25 10:29:21 +0100

    load footer links from CDN

--- config.rb
+++ config.rb
@@ -9,18 +9,17 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
+end
+
+# Load data
+activate :data_source do |c|
+  c.root = "#{ENV['CDN_URL']}/data"
+  c.files = [
+    "links.json"
+  ]
 end
 
 # Set markdown template engine
@@ -32,7 +31,8 @@ set :markdown, smartypants: true
 ###
 
 # use asset host
-# activate :asset_host, host: "//#{ENV['CDN_HOST']}"
+# activate :asset_host, host: ENV['CDN_URL']
+
 activate :dotenv
 
 # Methods defined in the helpers block are available in templates
